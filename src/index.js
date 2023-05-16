1bba1a005885cb4748d6cae82fb1d5495cc7273d
jatinkh25 <jatinkhatri25.jk@gmail.com>
2023-05-17 01:11:01 +0530

    firsinitial code pushed

---
+++ src/index.js
@@ -0,0 +1,94 @@
+import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import { spawn } from 'child_process'
+import fs from 'fs'
+import readline from 'readline'
+
+const app = express()
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+  },
+})
+
+io.on('connection', (socket) => {
+  // Specify the file path
+  // const filePath = '/home/justone/Desktop/tremor-detector/backend/src/test.txt'
+
+  // // Read the file
+  // fs.readFile(filePath, 'utf8', (err, data) => {
+  //   if (err) {
+  //     console.error(err)
+  //     return
+  //   }
+
+  //   // File contents are available in the 'data' parameter
+  //   console.log(data)
+  //   socket.emit('get-delta', data)
+  // })
+
+  // Specify the file path
+  const filePath = '/home/justone/Desktop/tremor-detector/backend/src/Data_test.csv'
+
+  // Create a readable stream from the file
+  const stream = fs.createReadStream(filePath)
+
+  // Create a readline interface
+  const rl = readline.createInterface({
+    input: stream,
+    crlfDelay: Infinity,
+  })
+
+  // Read the file line by line
+  rl.on('line', (line) => {
+    io.emit('get-delta', line)
+  })
+
+  // Handle end of file
+  rl.on('close', () => {
+    console.log('End of file')
+  })
+})
+
+app.post('/iot', (req, res) => {
+  // Convert the data to a string
+  const dataString = JSON.stringify(postData)
+
+  // Define the file path and name
+  const filePath = 'data.txt'
+
+  // Write the data to the file
+  fs.writeFile(filePath, dataString, (err) => {
+    if (err) {
+      console.error('Error writing file:', err)
+      res.status(500).json({ error: 'Failed to write file' })
+    } else {
+      res.status(200).json({ message: 'Post request successful' })
+      console.log('Data written to file')
+      const pythonFile = spawn('python3', ['src/notebook.py'])
+
+      pythonFile.stdout.on('data', (classifierOutput) => {
+        console.log(`stdout: ${classifierOutput}`)
+        io.emit('get-delta', { iotData: req.body, classifierOutput })
+      })
+
+      pythonFile.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`)
+      })
+
+      pythonFile.on('close', (code) => {
+        console.log(`child process exited with code ${code}`)
+      })
+    }
+  })
+
+  res.status(200).json({ message: 'Post request successful' })
+})
+
+httpServer.listen(process.env.PORT || 3001, () => {
+  console.log('listening on port 3001')
+})
---
+++ src/notebook.py
@@ -0,0 +1,53 @@
+# !pip install pandas
+import pandas
+import pickle
+import csv
+import sklearn
+
+'''Open text file'''
+f = open("/home/justone/Desktop/tremor-detector/backend/src/test.txt", "r")
+
+'''Open csv database'''
+filename = open("Data_test.csv", "w")
+
+fields = ['aX', 'aY', 'aZ', 'gX', 'gY', 'gZ', 'mX', 'mY', 'mZ']
+
+'''Saving text data into csv database'''
+with filename as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(fields)
+    for line in f:
+        l = line
+        vals = l.split(',')
+        for i in range(len(vals)):
+            vals[i] = float(vals[i])
+        csvwriter.writerow(vals)
+
+f.close()
+filename.close()
+
+file = open("Data_test.csv", "r")
+with file as f:
+    reader = csv.reader(f)
+    next(reader)
+    data = []
+    for row in reader:
+        if len(row) > 0:
+            data.append({"val": [float(cell) for cell in row]})
+
+
+val = [row["val"] for row in data]
+'''Loading pre-trained model'''
+with open('/home/justone/Desktop/tremor-detector/backend/src/RandomForestClassifier()model.pickle', 'rb') as f:
+    model = pickle.load(f)
+'''Testing only last 20 values'''
+test_data = [i for i in val[len(data)-20:]]
+
+'''Predicting Result'''
+result = model.predict(test_data)
+count = sum(result)
+
+if count >= 11:
+    print("shaking")
+else:
+    print("stable")
